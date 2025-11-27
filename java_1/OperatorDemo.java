1d1516e1a0b9c398b2c71b3aefa43f25273d9023
dhanushr9625 <dhanushdhanush3579@gmail.com>
2025-11-27 17:15:36 +0530

    Add files via upload

---
+++ java_1/OperatorDemo.java
@@ -0,0 +1,14 @@
+class OperatorDemo {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a > b : " + (a > b));
+
+        boolean x = true, y = false;
+        System.out.println("x && y = " + (x && y));
+
+        int max = (a > b) ? a : b;
+        System.out.println("Max of a and b = " + max);
+    }
+}
